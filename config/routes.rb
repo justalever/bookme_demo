ca98e8dbc3099f80359071cc6114f8fe2e36726d
Andy Leverenz <andy@justalever.com>
2022-08-17 15:51:51 -0500

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+require 'sidekiq/web'
+
+Rails.application.routes.draw do
+    authenticate :user, lambda { |u| u.admin? } do
+      mount Sidekiq::Web => '/sidekiq'
+    end
+
+
+  devise_for :users
+  root to: 'home#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
